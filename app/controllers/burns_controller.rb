3bd2bcf7ae3154a24aeb74d9618104fad8627fa8
aracelicaes <aracelicaes@gmail.com>
2020-09-02 15:09:10 -0500

    Create Registrations Controller Created registrations_controller.rb Add #sign_up_params & #account_update_params Customized User Route to Registrations Controller

--- app/controllers/burns_controller.rb
+++ app/controllers/burns_controller.rb
@@ -1,5 +1,6 @@
 class BurnsController < ApplicationController
   before_action :set_burn, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /burns
   # GET /burns.json
