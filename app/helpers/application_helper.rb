c5fa9ce987f13f8716d6cd9b7f5b79d6c678f449
jessicafarias <jessica.farias.rosado@gmail.com>
2020-09-03 17:03:54 -0500

    Adding flash_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def display_propper_flash
+    if flash[:alert]
+      content_tag(:div, flash[:alert], class: 'alert alert-danger', align: 'center')
+    elsif flash[:notice]
+      content_tag(:div, flash[:notice], class: ' alert alert-primary', align: 'center')
+    elsif flash[:danger]
+      content_tag(:div, flash[:danger], class: 'flash danger', align: 'center')
+    end
+  end
 end
