3566e7f47b49870a0d3f067dc51d8fa6d9fb9c4b
aracelicaes <aracelicaes@gmail.com>
2020-09-04 11:05:47 -0500

    Creating burns_helper methods for icon links

--- app/helpers/burns_helper.rb
+++ app/helpers/burns_helper.rb
@@ -1,40 +1,49 @@
 module BurnsHelper
   def user_image_display
-    if user_signed_in?
-      content_tag(:div) do
-        gravatar_image_tag(current_user.email, size:64, alt: current_user.name)
-      end
+    return unless user_signed_in?
+
+    content_tag(:div) do
+      gravatar_image_tag(current_user.email, size: 64, alt: current_user.name)
     end
   end
 
   def user_data_display(burn)
-    if user_signed_in?
-      content_tag(:div, class: 'row') do
-        content_tag(:span) do
-          burn.user.name
-        end
-      end +
+    return unless user_signed_in?
+
+    content_tag(:div, class: 'row') do
+      content_tag(:span) do
+        burn.user.name
+      end
+    end +
       content_tag(:div, class: 'row') do
         content_tag(:span) do
           burn.user.username
         end
       end
-    end
   end
 
+  def user_link_display(burn)
+    return unless user_signed_in? && current_user.id == burn.user.id
 
-  # <% if user_signed_in? && current_user.id == burn.user.id %>
-  #   <div class="row">
-  #     <%= link_to burn, class: "level-item mr-2" do %>
-  #       <span class="icon"><i class="fa fa-link" aria-hidden="true"></i></span>
-  #     <% end %>
-
-  #     <%= link_to edit_burn_path(burn), class: "level-item mr-2" do %>
-  #       <span class="icon"><i class="fa fa-pencil" aria-hidden="true"></i></span>
-  #     <% end %>
-  #     <%= link_to burn, method: :delete, data: { confirm: "Are you sure you want to delete this burn?" } do %>
-  #         <span class="icon mr-2"><i class="fa fa-trash-o" aria-hidden="true"></i></span>
-  #     <% end %>
-  #   </div>
-  # <% end %>
+    content_tag(:div, class: 'row') do
+      content_tag(:span) do
+        link_to burn, class: 'level-item mr-2' do
+          content_tag(:i, class: 'fa fa-link') do
+          end
+        end
+      end +
+        content_tag(:span) do
+          link_to edit_burn_path(burn), class: 'level-item mr-2' do
+            content_tag(:i, class: 'fa fa-pencil') do
+            end
+          end
+        end +
+        content_tag(:span, class: 'icon mr-2') do
+          link_to burn, method: :delete, data: { confirm: 'Are you sure you want to delete this burn?' } do
+            content_tag(:i, class: 'fa fa-trash-o') do
+            end
+          end
+        end
+    end
+  end
 end
